5e9af2ac9194c387056d08d41f02d4abf637dc9e
logan <Loganold@hotmail.com>
2025-02-10 10:49:50 -0500

    Fixed js folder?

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,8 +1,4 @@
 import {getLoginCookie} from "/js/attemptlogin.js";
 let userCookie = getLoginCookie();
 
-function hideDiv(element) {
-
-}
-
 document.getElementById("landing-page-welcome").innerHTML = "Welcome " + userCookie;
