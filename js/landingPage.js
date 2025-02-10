db59c0005aa392002f0658132acbb672235c19c8
logan <Loganold@hotmail.com>
2025-02-10 11:30:38 -0500

    Fixed landing_page.html linking issue? 4

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,4 +1,4 @@
-import {getLoginCookie} from "./js/attemptlogin.js";
+import {getLoginCookie} from "./haloman3069.github.io/js/attemptlogin.js";
 let userCookie = getLoginCookie();
 
 document.getElementById("landing-page-welcome").innerHTML = "Welcome " + userCookie;
