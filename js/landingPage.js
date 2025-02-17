763067fbb04910fcbe52ba95c21b4e4f3e19c9d5
logan <Loganold@hotmail.com>
2025-02-17 17:23:41 -0500

    big update added the first app design assignment and updated some web dev pages

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,4 +1,12 @@
 import {getLoginCookie} from "./attemptlogin.js";
+
 let userCookie = getLoginCookie();
 
-document.getElementById("landing-page-welcome").innerHTML = "Welcome " + userCookie;
+let userbar = document.getElementById("landing-page-welcome");
+if (userCookie) {
+    userbar.innerHTML = "Welcome " + userCookie;
+    userbar.classList.add("user-header");
+} else {
+    /* userbar.style.display = "none"; */
+    userbar.innerHTML = "No User Logged In!";
+}
